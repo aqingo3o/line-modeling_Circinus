c30fb3472015a834d67b1a10e7d646dee20c6638
Livia <universe5961@gmail.com>
2025-11-14 00:49:46 +0800

    it can work

--- pyradex_at-first-sight/fake-data.py
+++ pyradex_at-first-sight/fake-data.py
@@ -2,8 +2,10 @@
 
 import pyradex
 import numpy as np
+datapath = "/home/aqing/pyradex/pyradex_data/"
 
-# 建立一個假 pixel 陣列：4 pixels，各自有不同條件
+
+# fake pixel arrayyyy
 temp = [10, 20, 40, 80]  # K
 dens = [1e3, 1e4, 1e5, 1e6] # cm^-3
 column = [1e13, 1e14, 1e15, 1e16] # cm^-2
@@ -15,7 +17,22 @@ for T, n, N in zip(temp, dens, column):
         collider_densities={'H2': n},
         column=N,
         temperature=T,
-        datapath='/path/to/pyradex/data'  # 要改成你的路徑
+        datapath=datapath
     )
     output = R()
-    results.append(output['T_B'][0])  # 取出第一條線的亮度溫度
+    results.append(output['T_B'][0])  # maybe brightness temp?
+
+
+print('can be here')
+print(results)
+
+'''aqing 20151113
+okayy, the result can be printed so the code can work
+but i am not sure about the unit i've used
+and the data structure of the object "R"
+
+and i have to make sure how zip() actually work
+
+(for next time ) i want to use the data from my fits
+pick out some pixels and di the "modeling" 
+'''
