65285be23dd432e698eed3b3c106ecccc27ce804
Liv <universe5961@gmail.com>
2026-02-01 15:33:39 +0800

    the modi on Linux

--- scripts/preProcess/cube_cropper_casa.py
+++ scripts/preProcess/cube_cropper_casa.py
@@ -2,19 +2,16 @@
 # A script for full CASA
 '''
 Crop datacubes into smaller pieces.
-因為還有計算的需求所以會再補線兩
+因為還有計算噪音的需求所以會留下線兩端的空白區間哈哈屁眼
 '''
 import glob
 import shutil
 
-projectRoot = '/Users/aqing/Documents/1004/line-modeling_Circinus'
+projectRoot = '/home/aqing/Documents/line-modeling_Circinus'
 dataPath = f'{projectRoot}/data/alma_cube'
 cubes = glob.glob(f'{dataPath}/ori_cube/cube_*.fits')
 
-for i in cubes:
-    print(i)
-
-wanted_channs = [ # (mole, channels(casaForm))
+wanted = [ # (mole, channels(casaForm, check from CARTA))
     ('13co-10', '200~1200',  '3a'),
     ('13co-21', '500~2200',  '6a'),
     ('c18o-21', '2100~3500', '6a'),
@@ -23,16 +20,18 @@ wanted_channs = [ # (mole, channels(casaForm))
     ('c18o-10', '2600~3800', '6a'), # 按照 cubes 的順序的啊
 ]
 
-for pathIN in cubes:
+for i in range(len(cubes)):
+    pathIN = cubes[i]
     importfits(fitsimage=pathIN, imagename='casaIN.im', overwrite=True)
     print('Successfully import a datacube.')
-
-    for filename, channs, band in wanted_channs:
-        outfile = f'{dataPath}/cropped_cube/cube_Band{band}_{filename}_cropped.fits'
-        imsubimage(imagename='casaIN.im', outfile='casaOUT.im', chans=channs)
-        exportfits(imagename='casaOUT.im', fitsimage=outfile, overwrite=True)
-        shutil.rmtree('casaOUT.im')
-        print(f'{outfile} was done.')
-
+    filename = wanted[i][0]
+    channs = wanted[i][1]
+    band = wanted[i][2]
+    outfile = f'{dataPath}/cropped_cube/cube_Band{band}_{filename}_cropped.fits'
+    imsubimage(imagename='casaIN.im', outfile='casaOUT.im', chans=channs)
+    exportfits(imagename='casaOUT.im', fitsimage=outfile, overwrite=True)
+    shutil.rmtree('casaOUT.im')
+    print(f'{outfile} was done.')
     shutil.rmtree('casaIN.im')
-    print('All finish :)')
+
+print('All finish :)')
