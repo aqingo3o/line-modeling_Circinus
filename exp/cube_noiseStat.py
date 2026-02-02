0c6e9eee0664673db558610b00e1d0acdddb38de
Liv <universe5961@gmail.com>
2026-02-02 19:11:49 +0800

    staged success :)
    終於不再 nan
    但是只跑一個分子，不知道巢有沒有出錯哈哈

--- exp/cube_noiseStat.py
+++ exp/cube_noiseStat.py
@@ -3,58 +3,64 @@
 '''
 How much Jy/beam of noise should be masked when generating mom0.  
 the datacubes had been cropped by CASA ({projectRoot}/data/alma_cube/cropped_cube)
-(can't do this on feifei due to some cubes are too big.)
+(can't do crop on feifei due to some cubes are too big.)
 '''
 
-from astropy.io import fits
 from astropy import units as u
-from astropy.io import fits
-from astropy.wcs import WCS
 import numpy as np
 from spectral_cube import SpectralCube
+import warnings
 
-projectRoot = '/Users/aqing/Documents/1004/line-modeling_Circinus'
+# 因為噴一堆東西有點煩煩的
+warnings.filterwarnings('ignore', message='.*PV2.*')
+
+# Path
+projectRoot = '/Users/aqing/Documents/1004/line-modeling_Circinus' # 因為不知道校本確切來說會放哪所以這樣比較順暢吧
 dataPath = f'{projectRoot}/data/alma_cube/cropped_cube'
 
 # (mole_fileName, band_fileName, (blankChannel))
-moles_info = [
-    ('co-10', '3b', ()),
-    ('co-21', '6a', ()),
-    ('13co-10', '3a', ()),
-    ('13co-21', '6a', ()),
-    ('c18o-10', '3a', ()),
-    ('c18o-21', '6a', ()),
+moles_info = [ # for test
+    ('13co-10', '3a', (56, 316, 721, 900)),
 ]
-radii_step = 0.5 * u.arcsec
 
 noiseList = []
 radiiList = []
-'''
-# noiseList
-[[co-10在各種尺寸遮罩下的噪音], [13co-10在各種尺寸遮罩下的噪音], [c18o-10在各種尺寸遮罩下的噪音], ...]
-# radiiList
-[[co-10的各種尺寸的遮罩], [13co-10的各種尺寸的遮罩], [c18o-10的各種尺寸的遮罩], ...]
-'''
 
+# Main
+radii_step = 0.5 * u.arcsec # 每 0.5 arcsec 做一個 mask
 for molename, band, cblank in moles_info:
-    cube = (f"{dataPath}/cube_Band{band}_{molename}_cropped.fits")
+    cube = SpectralCube.read((f"{dataPath}/cube_Band{band}_{molename}_cropped.fits"))
+    print(f'cube_Band{band}_{molename}_cropped.fits was loaded.')
+
     ra_crval  = cube.wcs.wcs.crval[0] * u.deg
     dec_crval = cube.wcs.wcs.crval[1] * u.deg
-    ra_mat, dec_mat = cube.spatial_coordinate_map # 回傳 ra, dec 矩陣
+    dec_cr_rad = dec_crval.to(u.rad) # 修正投影用的, Circinus 有點高緯
 
-    dist_mat = np.sqrt((ra_mat-ra_crval)**2 + (dec_mat-dec_crval)**2).to(u.arcsec)
-    radii_fov = (0.5(ra_mat.max() - ra_mat.min())).to(u.arcsec)
-    
-    noiseList_mole = [] # 次拋, 裝的是一個 mole 在各種遮罩下的噪音
-    radiiList_mole = np.arange(1, radii_fov, step=radii_step)
+    dec_mat, ra_mat = cube.spatial_coordinate_map
+    delta_dec = (dec_mat - dec_crval)
+    delta_ra = (ra_mat - ra_crval) * np.cos(dec_cr_rad)
+    dist_mat = np.sqrt(delta_ra**2 + delta_dec**2).to(u.arcsec)
+    fov_r = (0.5*(dec_mat.max() - dec_mat.min())).to(u.arcsec) # field of view 的 ridius 的意思
+
+    noiseList_mole = []
+    radiiList_mole = np.arange(1, fov_r.value, step=radii_step.value)
     for r in radiiList_mole:
-        region = dist_mat <= r
-        noise = 0
+        mask = dist_mat.value <= r # <= 的標示為 True
+        cube_masked = cube.with_mask(mask)
+        noise = 0 * (u.Jy/u.beam) # 歸零，放個單位
+#'''
         for c in cblank:
-            noise += cube[c].with_mask(region).flattened_data().std()
-        noiseList_mole.append(noise / len(cblank))
+            noise = np.nanstd(cube_masked[c].filled_data[:])
+            '''# 他媽的這邊我用超久
+            cube[c] 就是 cube 的第c個 channel
+            filled_data[:] 像是取出所有值並轉職成一維陣列？
+            因為充滿 nan 所以要用 np.nanstd()
+            '''
+        noiseList_mole.append(noise / len(cblank)) # avg
 
     noiseList.append(noiseList_mole)
-    radiiList.append(radiiList_mole) # 寫在這邊比較對稱哈哈
-
-
+    radiiList.append(radiiList_mole)
+#'''
+print(noiseList)
+print(len(noiseList[0]))
+print('finalllllllllly')
--- exp/cube_noiseStat_fullComment.py
+++ exp/cube_noiseStat_fullComment.py
@@ -6,10 +6,7 @@ the datacubes had been cropped by CASA ({projectRoot}/data/alma_cube/cropped_cub
 (can't do crop on feifei due to some cubes are too big.)
 '''
 
-from astropy.io import fits
 from astropy import units as u
-from astropy.io import fits
-from astropy.wcs import WCS
 import numpy as np
 from spectral_cube import SpectralCube
 import warnings
@@ -37,7 +34,6 @@ moles_info = [ # for test
     ('13co-10', '3a', (56, 316, 721, 900)),
 ]
 
-
 noiseList = []
 radiiList = []
 '''
@@ -47,10 +43,11 @@ radiiList = []
 [[co-10的各種尺寸的遮罩], [13co-10的各種尺寸的遮罩], [c18o-10的各種尺寸的遮罩], ...]
 '''
 
+# Main
 radii_step = 0.5 * u.arcsec # 每 0.5 arcsec 做一個 mask
 for molename, band, cblank in moles_info: # cblank 是一個長度4的channel串列，標示空的channel
     cube = SpectralCube.read((f"{dataPath}/cube_Band{band}_{molename}_cropped.fits"))
-    print(f'cube_Band{band}_{molename}_cropped.fits was opened.')
+    print(f'cube_Band{band}_{molename}_cropped.fits was loaded.')
 
     # 以中心為中心畫圓
     ra_crval  = cube.wcs.wcs.crval[0] * u.deg # ra213, dec-65, print(cube)說的, 也可以看 carta
@@ -64,19 +61,17 @@ for molename, band, cblank in moles_info: # cblank 是一個長度4的channel串
     *_crval 系列也都是 deg 為單位, 所以等下可以運算每點與中心(*_crval)距離
     '''
 
+    delta_dec = (dec_mat - dec_crval) # 跨越緯線的長度(在兩條尾線之間移動)不會變, 所以就一般一般
     delta_ra = (ra_mat - ra_crval) * np.cos(dec_cr_rad) 
     '''# 乘上中心點的緯度 (仰角) 作為修正
     高緯度的東西跨越經線的時候走的距離比較短
     極點 (90) 的時候跨越經線(delta_ra)的距離為零
     '''
-    delta_dec = (dec_mat - dec_crval) # 跨越緯線的長度(在兩條尾線之間移動)不會變, 所以就一般一般
     dist_mat = np.sqrt(delta_ra**2 + delta_dec**2).to(u.arcsec) # 各點與中心(_crval)的距離矩陣, 都換成角秒
  
     fov_r = (0.5*(dec_mat.max() - dec_mat.min())).to(u.arcsec) # field of view 的 ridius 的意思
-
     '''# 座標最大減最小 的一半 就是整個視野的半徑
-    用 dec 算因為他不會變形
-    用 ra 也可以啦但要乘 cos(dec_cr_rad)
+    用 dec 算因為他不會變形, 用 ra 也可以啦但要乘 cos(dec_cr_rad)
 
     原本的單位是 deg, 用 .to() 換成角秒
 
@@ -89,8 +84,7 @@ for molename, band, cblank in moles_info: # cblank 是一個長度4的channel串
     會跟 carat 看起來有差是因為他媽的外面那圈 nan(底色) 也是在數據的範圍內啊
     image 其實是正方形, 操
 
-    我的理念就是, 邊緣圈到一堆 nan 的話一定會很醜
-    那我就知道這些是爛數據了
+    我的理念就是, 邊緣圈到一堆 nan 的話一定會很醜, 那我就知道這些是爛數據了
     '''
     
     noiseList_mole = [] # 次拋, 裝的是一個 mole 在各種遮罩下的噪音
@@ -100,22 +94,32 @@ for molename, band, cblank in moles_info: # cblank 是一個長度4的channel串
     '''
 
     for r in radiiList_mole:
-        region = dist_mat.value <= r # <= 的標示為 True
-        cube_masked = cube.with_mask(region)
+        mask = dist_mat.value <= r # <= 的標示為 True
+        cube_masked = cube.with_mask(mask)
         '''#做 masking
-        用 .with_mask(), 括號中間填入遮罩物件(region, mask 的意思,但繼承 carta 所以叫他 region)
-        因為 mask 只作用在 cube 上, 所以要先 mask 再切片
+        用 .with_mask(), 括號中間填入遮罩物件(mask 本人)
+        因為 .with_mask() 只作用在 cube 上, 所以要先 mask 再切片
+        
+        mask 只是把 mask 外的東西標記為 nan, 並沒有真的去「切」影像
+        所以
+        print(cube)
+        print(cube_masked)
+        會看起來一樣
         '''
         noise = 0 * (u.Jy/u.beam) # 歸零，放個單位
-'''
+#'''
         for c in cblank:
-            noise += cube_masked[c].filled_data[:].std()
-            # cube[c] 就是 cube 的第c個 channel
-            # ._data().std()
+            noise = np.nanstd(cube_masked[c].filled_data[:])
+            '''# 他媽的這邊我用超久
+            cube[c] 就是 cube 的第c個 channel
+            filled_data[:] 像是取出所有值並轉職成一維陣列？
+            因為充滿 nan 所以要用 np.nanstd()
+            '''
         noiseList_mole.append(noise / len(cblank)) # avg
 
     noiseList.append(noiseList_mole)
     radiiList.append(radiiList_mole) # 寫在這邊比較對稱哈哈
-'''
+#'''
 print(noiseList)
-print('done here')
+print(len(noiseList[0]))
+print('finalllllllllly')
