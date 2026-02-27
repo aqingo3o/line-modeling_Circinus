4e311b34e13d2171b8d556e9fbc7e9c20d077e77
Liv <universe5961@gmail.com>
2026-02-27 22:03:54 +0800

    plz check if the cube is broken after smooth
    在前面的註解加入了新的警告
    說 smooth 完記得要檢查 cube 是否損壞

--- scripts/preProcess/cube_smoother_casa.py
+++ scripts/preProcess/cube_smoother_casa.py
@@ -2,12 +2,16 @@
 # A script for full CASA
 '''
 又來到的熟悉的 convole to the same beam 
+依然選擇了熟悉的 CASA
 
-!! 如果這個腳本運行到一半 casa 就被 killed
+!! 如果這個腳本運行到一半 CASA 就被 killed
 可能是因為 ram 爆炸了 (Linux terminal: )
 在終端先輸入 export OMP_NUM_THREADS=4
 再進入 casa 就可以解決ヽ(｀▽´)ノ 但運行時間當然會加倍啦
 總之不要塞滿執行緒, Lab 那台好像有 12 緒吧, 全塞滿的話大 cube 會爆 ram
+
+!! 非常之建議做完 smooth 之後要用 CARTA 或是其他可以看光譜軸的方法確認 cube 有沒有壞掉,
+(有前科的) 可能會有譜線強度在某個 channel 之後全部歸零的情況, 個人猜測很高機率是硬體的鍋
 '''
 #from casatasks import importfits, imsmooth, exportfits
 import shutil
