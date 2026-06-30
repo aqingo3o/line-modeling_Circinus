a09b296f3d0e9cb8433a33530391870ee0b6b4fb
Liv <universe5961@gmail.com>
2026-06-30 23:27:58 +0800

    Use casatools to modify header key ['restfreq']
    Use casatools to modify header key ['restfreq'] and increase the recognition of status hints.

--- scripts/preProcess/cube_cropper_casa.py
+++ scripts/preProcess/cube_cropper_casa.py
@@ -1,23 +1,23 @@
-# from github: Hello_Circinus/miniscripts
 # A script for full CASA
 '''
-Cut datacubes into smaller pieces and revise the header keyword 'RESTFRQ'
+Cut datacubes into smaller pieces and revise the header keyword 'RESTFRQ' by casatools (.setrestfrequency())
 Leave at least 1x width as the spectral line in both sides for error estimation.
+
+update: 2026-06-30, Use casatools to modify header key ['restfreq']
+                    and increase the recognition of status hints.
 '''
-#from casatasks import importfits, imsubimage, imhead, exportfits
 import glob
 import shutil
 
 # Files and Path
-#projectRoot = '/Users/aqing/Documents/1004/line-modeling_Circinus' # feifei, for testing
-projectRoot = '/home/aqing/Documents/line-modeling_Circinus' # for Lab Machine
+projectRoot = '/home/aqing/Documents/line-modeling_Circinus' # for Lab Machine or blackhole
 dataPath = f'{projectRoot}/data/alma_cube'
 cubes = glob.glob(f'{dataPath}/ori_cube/cube_*.fits')
 cubes.sort() # make sure the order is the same in every computer!
 
 # (molename, band, sub-image's channel(casaForm, check from CARTA), restFrequency(Hz))
-wanted = [ 
-    ('c18o-10', '3a', '0~1', 1.09782E+11), # c18o-10 is not used in modeling
+wanted = [
+    #('c18o-10', '3a', '0~1', 1.09782E+11), # c18o-10 is not used in modeling
     ('13co-10', '3a', '10~1870',   1.10201E+11),
     ('co-10',   '3b', '1035~3440', 1.15271E+11),
     ('c18o-21', '6a', '1000~3810', 2.19560E+11),
@@ -26,7 +26,7 @@ wanted = [
     ('co-32',   '7',  '70~415',    3.45796E+11),
 ]
 
-# Cropping
+# Main
 for i in range(len(cubes)):
     pathIN = cubes[i]
     mole = wanted[i][0]
@@ -36,16 +36,23 @@ for i in range(len(cubes)):
     pathOUT = f'{dataPath}/cropped_cube/cube_Band{band}_{mole}_cropped.fits'
 
     importfits(fitsimage=pathIN, imagename='casaIN.im', overwrite=True)
-    print('Successfully import a datacube.')
+    print(f">>aqing : Successfully load {mole}'s cube.")
 
+    # Crop
     imsubimage(imagename='casaIN.im', outfile='casaCUT.im', chans=spchanns)
-    imhead(imagename='casaCUT.im',
-           mode='put', hdkey='restfrq', hdvalue=str(f0)) # 資料型態竟然要是字串，不客氣，幫轉了
-    print(f"Finish cropping {mole} and the header keyWord 'RESTFRQ' now is {f0} Hz.") 
+
+    # Change Restfreequency, by casatools
+    ia.open('casaCUT.im')
+    csys = ia.coordsys()
+    csys.setrestfrequency(value=f0)
+    ia.setcoordsys(csys.torecord())
+    ia.done()
+
+    print(f">>aqing : Finish cropping {mole} and the header keyWord 'RESTFRQ' now is {f0} Hz.")
 
     exportfits(imagename='casaCUT.im', fitsimage=pathOUT, overwrite=True)
     shutil.rmtree('casaCUT.im')
     shutil.rmtree('casaIN.im')
-    print(f"Finish processing {mole}'s original cube and metafiles are cleaned.")
+    print(f">>aqing : Finish processing {mole}'s original cube and metafiles are cleaned.")
 
 print('All done :)')
