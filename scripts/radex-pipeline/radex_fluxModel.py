1bfc39fa55eb102df43cf63641773d6ce3c6d34d
Liv <universe5961@gmail.com>
2026-08-10 15:14:34 +0900

    temporary backup

--- scripts/radex-pipeline/radex_fluxModel.py
+++ scripts/radex-pipeline/radex_fluxModel.py
@@ -1,19 +1,13 @@
-# Can be run on Lab machine(ubuntu) or feifei(macOS)
-# Auto set the folder structure
-# You MUST put this script under your project root (i.e. ~/line-modeling_Circinus/thisScript.py)
-# 對如果要把這個腳本放進 projectRoot/scripts/ 的話, 請跑完之後再放, 雖然很怪洨但先這樣吧?
+# Script for feifei or blackhole.
+# Put this script under {projectRoot}, (i.e. /home/aqing/Documents/line-modleing_Circiuns/)
+# Remaining dependency paths will be created automatically.
 '''
-update: 2026-07-28, Change physical condition range, find history on my github.
-                    New model is save to folders named "_iset"!
+因為一些發現, 總之要自己寫這個程式了, 不知道是大事很妙還是大事不妙
+雖然這個程式並非出現在工作的前期 (也就是我已經擁有熟成的檔案結構)
+但為了後續復現的方便 && 這隻程式高度資料夾路徑依賴
+為了防止程式開始跑了才發現很多東西不存在, 我保留了自動建立檔案結構的部分。
 
 程式湯底來自 Eltha 女士的 radex_pipeline.py, flux_model_6d.py, 
-對標寒假的工作算是 radex-pipeline_setFolder-modiPath-add6d.py, 
-盡量的改變了檔案命名的方式, 但因為牽涉到平行處理, 有些地方我不敢亂動
-果然不是親自寫的程式用起來多半有點不安心...
-
-最前面的部分包含了設定整個工作空間的資料夾結構 (make_projectRoot.py && make_folders.py)
-因為 radex-pipeline 系列的工作會有很多寫入檔案的機會, 所以對於相同命名的環境要求有點多
-因此, 就算還沒有開始處理 cube, moment maps, 也可以先跑這支程式喔
 ----------------------------------------------------------------------------------
 然後這支程式真的狗幹長, 所以提供了[目錄]
 - Import Module
@@ -39,6 +33,7 @@ update: 2026-07-28, Change physical condition range, find history on my github.
 
 ### Write Time Records
 '''
+
 # ------------------------------- Import Module ------------------------------- #
 from joblib import Parallel, delayed
 import numpy as np
@@ -48,106 +43,122 @@ import time
 
 # -------------------------- Build Folder Structure ---------------------------- #
 '''
-print('Start building suitable folder structure for radex pipeline...')
+print('Start creating folder structure for radex_fluxModel.py ...')
 projectRoot = Path(__file__).resolve().parents[0] # line-modeling_Circinus, no slash
-projectRoot_member = ['data', 'docs', 'exp', 'products', 'scripts', 'src'] # first-level
-print('Strat building projectRoot and the top-level subfolders...')
-for i in projectRoot_member: # first-level
+# First-level
+projectRoot_member = ['data', 'docs', 'exp', 'products', 'scripts'] 
+for i in projectRoot_member:
     projectRoot_sub = f'{projectRoot}/{i}'
     if not os.path.exists(projectRoot_sub):
         os.makedirs(projectRoot_sub)
-
+# Second-level
 print('Strat building second & third-level subfolders under projectRoot...')
-under_data = ['radex_io', 'model_npy'] # second-level
-under_radexio = ['input_5d-coarse2_co', 'input_5d-coarse2_13co', 'input_5d-coarse2_c18o', 
-                  'output_5d-coarse2_co', 'output_5d-coarse2_13co', 'output_5d-coarse2_c18o'] # third-level
-for i in under_data: # second-level
+under_data = ['radex_io', 'model_npy'] 
+for i in under_data:
     dataPath_sub = f'{projectRoot}/data/{i}'
     if not os.path.exists(dataPath_sub):
         os.makedirs(dataPath_sub)
-for i in under_radexio: # third-level
-    ioPath_sub = f'{projectRoot}/data/radex_io/{i}' # here's a hard-coding
+# Third-level
+under_radexio = ['input_co',  'input_13co',  'input_c18o', 
+                 'output_co', 'output_13co', 'output_c18o'] 
+for i in under_radexio:
+    ioPath_sub = f'{projectRoot}/data/radex_io/{i}' # radex_io/ is a hard-coding
     if not os.path.exists(ioPath_sub):
         os.makedirs(ioPath_sub)
-print('Folder structure is ok :)')
+print('Dependency folder strucrure is now OK :D')
 print()
 '''
 
 # ------------------------------- Path Variables ---------------------------------- #
-projectRoot = '/home/aqing/Documents/line-modeling_Circinus'
-radexioPath = f'{projectRoot}/data/radex_io_iset' # 巨幅檔案數目的那個
-npyPath = f'{projectRoot}/data/model_npy_iset'    # extract flux 的那些
-model_5d = '5d-coarse2' # level3 那邊相依的, 至今未知用途...
-model_6d = '6d-coarse2'
+projectRoot = '/home/aqing/Documents/line-modeling_Circinus' # blackhole
+projectRoot = '/Users/aqing/Documents/1004/line-modeling_Circinus' # feifei
+radexioPath = f'{projectRoot}/data/radex_io' # a VAST number of files
+npyPath = f'{projectRoot}/data/model_npy'    # extracted flux model
+d_with_bf = '5d' # model's dimensiom with beam filling factor
 
 ### ------------------------------ RADEX Pipeline ------------------------------- ###
 start_time = time.time()
 # -------------------------------- Basic Variables -------------------------------- #
 num_cores = 20 # joblib
 linewidth = 300 # km/s
-molecule_0 = 'co'
-molecule_1 = '13co'
-molecule_2 = 'c18o'
+mole0 = 'co'
+mole1 = '13co'
+mole2 = 'c18o'
 
-# ----------------------------- Physical Conditions Grid ---------------------------- #
-Nco = np.arange(12., 21.1, step=0.2)
-Tkin = np.arange(1., 3.1,  step=0.1)
-nH2 = np.arange(2.,  7.1,  step=0.2) # step size for Nco and nH2 should be the same
+# ------------------------- Set Physical Conditions Range ---------------------------- #
+'''
+就這邊的東西可以改,
+應該說下面可以改的東西就剩檔名了
+'''
+expstep_Tk = 0.1
+expstep_nH2 = 0.2
+expstep_Nco = expstep_nH2 # step size for Nco and nH2 should be the same (idky)
+'''
+stepex_*: 指數部分的 step
+i.e. Tkin -> 10^1, 10^1.1, 10^1.2, ... 10^2.7
+'''
+'''
+Tk_exp = np.arange(1., 2.8,  step=expstep_Tk)    # 前面的經驗說, 要從整數開始 but idky
+nH2_exp = np.arange(2.,  6.1,  step=expstep_nH2) # 多的那 .1 是因為 arange() 會在終點前停下
+Nco_exp = np.arange(15., 20.1, step=expstep_Nco) # 多家的那一點(小於step)是為了確保能停在預期的數字
+'''
+# FOR TEST
+Tk_exp = np.arange(1., 1.6,  step=expstep_Tk)
+nH2_exp = np.arange(2.,  3.7,  step=expstep_nH2)
+Nco_exp = np.arange(15., 16.1, step=expstep_Nco)
+# FOR TEST
+
+Tk_coe = np.arange(1, 10, step=1)
+nH2_coe = np.arange(1, 10, step=1)
+Nco_coe = np.arange(1, 10, step=1) # 先這樣試試?
+
 X_13co = np.arange(10, 126, step=10)
 X_c18o = np.arange(2, 21,   step=1)
-round_dens, round_temp = 1, 1
 
 # ----------------------------------- Pre-processing -------------------------------- #
-incr_dens = round(Nco[1] - Nco[0], 1)
-incr_temp = round(Tkin[1] - Tkin[0], 1)
-diff_Tk = Tkin[1] - Tkin[0]
-co_dex = np.round(10**np.arange(0., 1., incr_dens), 4)
-Tk_dex = np.round(10**np.arange(0., 1., incr_temp), 4)
 factors_13co = 1./X_13co  
 factors_c18o = 1./X_c18o
-num_Nco = Nco.shape[0]
-num_Tk = Tkin.shape[0]
-num_nH2 = nH2.shape[0]
-cycle_dens = co_dex.shape[0]
-cycle_temp = Tk_dex.shape[0]
-num_X12to13 = X_13co.shape[0]
-num_X13to18 = X_c18o.shape[0]
 
-# ------------------------------- def write_inputs_m*(): ------------------------------ #
-def write_inputs_m0(i,j,k):
-    powi = str(i//cycle_temp + int(Tkin[0]))
-    Tk = str(round(diff_Tk*i + int(Tkin[0]), round_temp))
-    powj = j//cycle_dens + int(nH2[0])
-    n_h2 = str(powj)
-    N_co = str(k//cycle_dens + int(Nco[0]))
+num_Tk_exp = len(Tk_exp)
+num_nH2_exp = len(nH2_exp)
+num_Nco_exp = len(Nco_exp)
+num_Tk_coe = len(Tk_coe)
+num_nH2_coe = len(nH2_coe)
+num_Nco_coe = len(Nco_coe)
+num_X1213 = len(X_13co)
+num_X1318 = len(X_c18o)
+
+# ------------------------------- def writeInputs_m*(): ------------------------------ #
+def writeInputs_m0(i, j, k):
+    pow_Tk = i//num_Tk_coe + int(Tk_exp[0])
+    Tk = round(expstep_Tk*i + int(Tk_exp[0]), 1)
+    pow_nH2 = j//num_nH2_coe + int(nH2_exp[0])
+    pow_Nco = k//num_Nco_coe + int(Nco_exp[0])
     
-    prei = str(Tk_dex[i%cycle_temp])
-    prej = str(co_dex[j%cycle_dens])
-    prej_r = str(round(co_dex[j%cycle_dens], round_dens))
-    prek = str(co_dex[k%cycle_dens])
-    codex = str(round(co_dex[k%cycle_dens], round_dens))
+    pre_Tk = Tk_coe[i%num_Tk_coe]
+    pre_nH2 = str(nH2_coe[j%num_nH2_coe])
+    pre_Nco = Nco_coe[k%num_Nco_coe]
 
-    file = open(f'{radexioPath}/input_{model_5d}_{molecule_0}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}.inp', 'w')
-    file.write(molecule_0+'.dat\n')
-    file.write(f'{radexioPath}/output_{model_5d}_{molecule_0}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}.out\n')
-    # 因為平行處理所以就不設 'filename' 這種多個函式會共用的變數了哈
-    file.write('100'+' '+'400'+'\n')
-    file.write(prei+'e'+powi+'\n')
+    file = open(f'{radexioPath}/input_{mole0}/{Tk}_{round(pre_nH2, 1)}e{pow_nH2}_{round(pre_Nco, 1)}e{pow_Nco}.inp', 'w')
+    file.write(f'{mole0}.dat\n')
+    file.write(f'{radexioPath}/output_{mole0}/{Tk}_{round(pre_nH2, 1)}e{pow_nH2}_{round(pre_Nco, 1)}e{pow_Nco}.out\n')
+    file.write('100 400\n')
+    file.write(f'{pre_Tk}e{pow_Tk}\n')
     file.write('1\n')
     file.write('H2\n')
-    file.write(prej+'e'+n_h2+'\n')
+    file.write(f'{pre_nH2}e{pow_nH2}\n')
     file.write('2.73'+'\n')
-    file.write(prek+'e'+N_co+'\n')
-    file.write(str(linewidth)+'\n')
+    file.write(f'{pre_Nco}e{pow_Nco}\n')
+    file.write(linewidth+'\n')
     file.write('0\n')
     file.close() 
 
 def write_inputs_m1(i,j,k,m):
-    powi = str(i//cycle_temp + int(Tkin[0]))
-    Tk = str(round(diff_Tk*i + int(Tkin[0]), round_temp))
-    powj = j//cycle_dens + int(nH2[0])
+    powi = str(i//cycle_temp + int(Tk_exp[0]))
+    Tk = str(round(expstep_Tk*i + int(Tk_exp[0]), round_temp))
+    powj = j//cycle_dens + int(nH2_exp[0])
     n_h2 = str(powj)
-    N_co = str(k//cycle_dens + int(Nco[0]))
+    N_co = str(k//cycle_dens + int(Nco_exp[0]))
     
     prei = str(Tk_dex[i%cycle_temp])
     prej = str(co_dex[j%cycle_dens])
@@ -156,9 +167,9 @@ def write_inputs_m1(i,j,k,m):
     x13co = str(X_13co[m])
     codex = str(round(co_dex[k%cycle_dens], round_dens))
 
-    file = open(f'{radexioPath}/input_{model_5d}_{molecule_1}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}.inp', 'w')
-    file.write(molecule_1+'.dat\n')
-    file.write(f'{radexioPath}/output_{model_5d}_{molecule_1}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}.out\n')
+    file = open(f'{radexioPath}/input_{mole1}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}.inp', 'w')
+    file.write(f'{mole1}.dat\n')
+    file.write(f'{radexioPath}/output_{mole1}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}.out\n')
     file.write('100'+' '+'400'+'\n')
     file.write(prei+'e'+powi+'\n')
     file.write('1\n')
@@ -166,16 +177,16 @@ def write_inputs_m1(i,j,k,m):
     file.write(prej+'e'+n_h2+'\n')
     file.write('2.73'+'\n')
     file.write(prek+'e'+N_co+'\n')
-    file.write(str(linewidth)+'\n')
+    file.write(linewidth+'\n')
     file.write('0\n')
     file.close()   
 
 def write_inputs_m2(i,j,k,m,n):
-    powi = str(i//cycle_temp + int(Tkin[0]))
-    Tk = str(round(diff_Tk*i + int(Tkin[0]), round_temp))
-    powj = j//cycle_dens + int(nH2[0])
+    powi = str(i//cycle_temp + int(Tk_exp[0]))
+    Tk = str(round(expstep_Tk*i + int(Tk_exp[0]), round_temp))
+    powj = j//cycle_dens + int(nH2_exp[0])
     n_h2 = str(powj)
-    N_co = str(k//cycle_dens + int(Nco[0]))
+    N_co = str(k//cycle_dens + int(Nco_exp[0]))
     
     prei = str(Tk_dex[i%cycle_temp])
     prej = str(co_dex[j%cycle_dens])
@@ -185,9 +196,9 @@ def write_inputs_m2(i,j,k,m,n):
     xc18o = str(X_c18o[n])
     codex = str(round(co_dex[k%cycle_dens], round_dens))
 
-    file = open(f'{radexioPath}/input_{model_5d}_{molecule_2}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}_{xc18o}.inp', 'w')
-    file.write(molecule_2+'.dat\n')
-    file.write(f'{radexioPath}/output_{model_5d}_{molecule_2}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}_{xc18o}.out\n')
+    file = open(f'{radexioPath}/input_{mole2}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}_{xc18o}.inp', 'w')
+    file.write(f'{mole2}.dat\n')
+    file.write(f'{radexioPath}/output_{mole2}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}_{xc18o}.out\n')
     file.write('100'+' '+'400'+'\n')
     file.write(prei+'e'+powi+'\n')
     file.write('1\n')
@@ -208,7 +219,7 @@ def run_radex_m0(i,j,k):
     
     prej_r = str(round(co_dex[j%cycle_dens], round_dens))
     codex = str(round(co_dex[k%cycle_dens], round_dens))
-    run = os.system(f'radex < {radexioPath}/input_{model_5d}_{molecule_0}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}.inp')
+    run = os.system(f'radex < {radexioPath}/input_{mole0}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}.inp')
     return run
 
 def run_radex_m1(i,j,k,m):
@@ -220,7 +231,7 @@ def run_radex_m1(i,j,k,m):
     prej_r = str(round(co_dex[j%cycle_dens], round_dens))
     x13co = str(X_13co[m])
     codex = str(round(co_dex[k%cycle_dens], round_dens)) 
-    run = os.system(f'radex < {radexioPath}/input_{model_5d}_{molecule_1}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}.inp')
+    run = os.system(f'radex < {radexioPath}/input_{mole1}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}.inp')
     return run
 
 def run_radex_m2(i,j,k,m,n):
@@ -233,9 +244,10 @@ def run_radex_m2(i,j,k,m,n):
     x13co = str(X_13co[m])
     xc18o = str(X_c18o[n])
     codex = str(round(co_dex[k%cycle_dens], round_dens))  
-    run = os.system(f'radex < {radexioPath}/input_{model_5d}_{molecule_2}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}_{xc18o}.inp')
+    run = os.system(f'radex < {radexioPath}/input_{mole2}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}_{xc18o}.inp')
     return run
 
+"""
 # --------------- Use Functions write_inputs_m*() for molecules0,1,2 ------------------- #
 print('Start to write .inp files...')
 Parallel(n_jobs=num_cores)(
@@ -302,9 +314,9 @@ def radex_flux(i,j,k,m,n):
     xc18o = str(X_c18o[n])
     codex = str(round(co_dex[k%cycle_dens], round_dens))
 
-    outfile_0 = f'{radexioPath}/output_{model_5d}_{molecule_0}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}.out'
-    outfile_1 = f'{radexioPath}/output_{model_5d}_{molecule_1}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}.out'
-    outfile_2 = f'{radexioPath}/output_{model_5d}_{molecule_2}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}_{xc18o}.out'
+    outfile_0 = f'{radexioPath}/output_{mole0}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}.out'
+    outfile_1 = f'{radexioPath}/output_{mole1}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}.out'
+    outfile_2 = f'{radexioPath}/output_{mole2}/{Tk}_{prej_r}e{n_h2}_{codex}e{N_co}_{x13co}_{xc18o}.out'
 
     # Extract reliable flux predictions (avoid those with convergence issues)
     if np.loadtxt(outfile_0, skiprows=10, max_rows=1, dtype='str')[3] == '****':
@@ -364,7 +376,7 @@ for result in results: # Get FluxxxxxX, by Function radex_flux()
             elif molesp == 'c18o':
                 flux_model[f'{molesp}-{t}']["flux"][k,i,j,m,n] = flux_data[m_idx][t_idx]
 '''
-保留這個東西因為這個太他媽懸吊了
+保留這個東西 因為這個太他媽懸吊了
 for result in results:
     k, i, j, m, n, flux_0, flux_1, flux_2 = result
     flux_co10[k,i,j] = flux_0[0]
@@ -397,7 +409,7 @@ for molesp, _ in mole_info: # reshape the initial flux model to 5d
         flux_model[f'{molesp}-{t}']["flux_5d"] = theFlux_5d
 
 for molename in flux_model.keys(): # Save "flux_5d" into .npy
-    np.save(f'{npyPath}/flux_{model_5d}_{molename}.npy', flux_model[molename]["flux_5d"]) # (filename) modi by qing (20260317)
+    np.save(f'{npyPath}/flux_{molename}.npy', flux_model[molename]["flux_5d"]) # (filename) modi by qing (20260317)
 
 ratio5d_time = time.time()
 
@@ -418,7 +430,7 @@ for molesp, _ in mole_info:
         flux_model[f'{molesp}-{t}']["flux_6d"] = theFlux_6d
 
 for molename in flux_model.keys(): # Save "flux_6d" into .npy
-    np.save(f'{npyPath}/flux_{model_6d}_{molename}.npy', flux_model[molename]["flux_6d"])
+    np.save(f'{npyPath}/flux_{d_with_bf}_{molename}.npy', flux_model[molename]["flux_6d"])
 print('Flux_6d models saved.')
 flux6d_time = time.time()
 
@@ -430,5 +442,5 @@ timerec.write(f'It took {(fluxini_time - radex_time):.2f} seconds to save 3d, 4d
 timerec.write(f'It took {(ratio5d_time - fluxini_time):.2f} seconds to save 5d ratio models.\n')
 timerec.write(f'It took {(flux6d_time - ratio5d_time):.2f} seconds to save 6d flux models.\n')
 timerec.close()
-
+"""
 print('Sincere congratulations! This script arrived here without any obstacles. <3')
