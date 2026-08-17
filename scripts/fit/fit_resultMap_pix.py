19d4483fd4c3a5e75823c5a22e0b3eb6238fcb74
Liv <universe5961@gmail.com>
2026-08-17 14:08:05 +0900

    show result map by my model grid

--- scripts/fit/fit_resultMap_pix.py
+++ scripts/fit/fit_resultMap_pix.py
@@ -1,70 +1,50 @@
 # Do this on feifei because I need mpl
-# Use the result of 'fit_fitMANYpix_(ugly or parallel).py'
-# ugly give partial map while parallel give wholemap.
+# Use the result of 'fit_fitMANYpix_parallel.py'
+# Please first scp back fitting results from blackhole if fitting was done on the server.
 '''
-Please first scp back fitting results from the server, 
-- map_chi2Min_{nline}line_{whichone}.npy
-- map_bestPhyCondi_{nline}line_{whichone}.npy
-
-Or get entire folder back by this command
-$scp -r aqing@140.xxx.xx.xx:~/Documents/line_modeling_Circinus/products/fittingResult_notsure ~/Desktop
-Then read them and make maps out of them.
+update: 2026-08-15, Use result from products/fittingReslut_newscript/
+                    , which is come from model grid I mdae by MYSELF (and best fit).
 '''
 import numpy as np
 import matplotlib.pyplot as plt
 
 projectRoot = '/Users/aqing/Documents/1004/line-modeling_Circinus' # feifei
-resultPath = f'{projectRoot}/products/fittingResult_notsure'
+resultPath = f'{projectRoot}/products/fittingReslut_newscript'
 
-# Choose which set of fitting result
-nline = '6line'
-whichone = 'wholemap' ###
-#whichone = 'partial' ###
+fitting_method = 'bestFit' # or 'MCMC', 'likkelyHood'
 
-if whichone == 'partial':
-    imrange = (340, 560)
-elif whichone == 'wholemap':
-    if nline == '5line':
-        imrange = (50, 840)
-    elif nline == '6line':
-        imrange = (0, 89)
-    #imrange = (200, 600)
+map_chi2_min = np.load(f'{resultPath}/map_chi2Min_4para.npy')
+map_best_phy = np.load(f'{resultPath}/map_bestPhyCondi_4para.npy')
 
-map_chi2_min = np.load(f'{resultPath}/map_chi2Min_{nline}_{whichone}.npy')
-map_best_phy = np.load(f'{resultPath}/map_bestPhyCondi_{nline}_{whichone}.npy')
-
-# 7 panels: 6 physical conditions + chi2
-phy_info = [ # Order matter!
-    # ('plot title', 'unit', 'cmap')
-    ('Column Density (Nco)',     r'log$N_{\rm CO}$ (cm$^{-2}$)',  'inferno'),
-    ('Kinetic Temperature (Tk)', r'log$T_{\rm k}$ (K)',           'hot'), 
-    ('Number Density (nH2)',     r'log$n_{\rm H_2}$ (cm$^{-3}$)', 'inferno'), 
-
-    ('12CO/13CO Abundance Ratio', '', 'jet'), 
-    ('13CO/C18O Abundance Ratio', '', 'jet'), 
-    ('Beam Filling Factor',       '', 'gray'),
+# 5 panels: 4 physical conditions + chi2
+# Order matter!
+phy_info = [
+    # ('plot title', (map range), 'unit', 'cmap')
+    ('Kinetic Temperature (Tk)', (10, 80),    'Kelvin',      'hot'), 
+    ('Number Density (nH2)',     (9e2, 9e4),     r'cm$^{-3}$', 'inferno'), 
+    ('Column Density (Nco)',     (1e19, 7e19), r'cm$^{-2}$', 'inferno'),
+    ('Beam Filling Factor',      (-0.1, 0.4),     '',            'gray'),
     ]
 
 # Plottinggg
-fig, ax = plt.subplots(2, 4, figsize=(20, 10)) # 存下來就好看了
+fig, ax = plt.subplots(2, 3, figsize=(14, 7)) # 存下來就好看了
 ax_flat = ax.flatten() # 壓成 1d 這樣可以用洄圈
-for i in range(7):
-    if i == 6: #chi2
-        splot = ax_flat[i].imshow(map_chi2_min[imrange[0]:imrange[1], imrange[0]:imrange[1]], 
-                                  vmax=6, origin='lower', cmap='tab10')
-        '''
-        cmap=tab10 雖然看起來很像嘔吐物, 但意外的很清楚耶耶
-        '''
+for i in range(5):
+    if i == 4: #chi2
+        splot = ax_flat[i].imshow(map_chi2_min, vmax=20,
+                                  origin='lower', cmap='tab10')
         cbar = fig.colorbar(splot, ax=ax_flat[i], fraction=0.046, pad=0.04)
-        ax_flat[i].set_title('chi2 min')
+        ax_flat[i].set_title('chi2')
+        print()
     else: # physical conditions
-        splot = ax_flat[i].imshow(map_best_phy[imrange[0]:imrange[1], imrange[0]:imrange[1], i], 
-                                  origin='lower', cmap=phy_info[i][2])
+        splot = ax_flat[i].imshow(map_best_phy[:, :, i], vmax=phy_info[i][1][1],
+                                  origin='lower', cmap=phy_info[i][3])
         cbar = fig.colorbar(splot, ax=ax_flat[i], fraction=0.046, pad=0.04) # 神奇小數值
         ax_flat[i].set_title(f'{phy_info[i][0]}')
-        cbar.set_label(phy_info[i][1])
-        
-ax_flat[-1].set_visible(False) # 新技術欸
-plt.tight_layout() # 神奇妙妙工具
-plt.savefig(f'{resultPath}/fig_fittingResult_{nline}_{whichone}.png', dpi=300, bbox_inches='tight')
+        cbar.set_label(phy_info[i][2])
+
+ax_flat[-1].set_visible(False) # 神奇妙妙工具1
+plt.tight_layout() # 神奇妙妙工具2
+plt.savefig(f'{resultPath}/fig_{fitting_method}_resultMap.png',
+            dpi=300, bbox_inches='tight')
 plt.show()
