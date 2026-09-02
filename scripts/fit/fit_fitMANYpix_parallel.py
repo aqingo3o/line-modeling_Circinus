0fb4190f7d6439601bba5f1c80f15e1e377f0aa9
Liv <universe5961@gmail.com>
2026-09-02 22:56:37 +0800

    Use resolved maps for fitting
    only 4 CO lines this time

--- scripts/fit/fit_fitMANYpix_parallel.py
+++ scripts/fit/fit_fitMANYpix_parallel.py
@@ -10,78 +10,101 @@ update: 2026-07-07, Revise the filename.
 	    2026-07-28, use mom0 maps that regrid by Nyquist sampling (0.1*beam size),
                     89*89 pix for each map...
         2026-07-29, change the caculation of 'best2phy()' to fit in new physical condition range.
+        2026-08-14, Use model by aqing with constant abundance ratio.
+        2026-09-02, Use resolved maps (0.41 arcsec) to fit, only 4 CO lines this time.
 '''
 
-### ----------------------------- Import Module ---------------------------- ###
+# ---------------------- Import Module ---------------------- #
 from astropy.io import fits
 from joblib import Parallel, delayed
 import numpy as np
 import time
 
 startTime = time.time()
-### --------------------------- Path Variables ----------------------------- ###
+# ---------------------- Path Variables ---------------------- #
 projectRoot = '/home/aqing/Documents/line-modeling_Circinus' # blackhole
-#projectRoot = '/Users/aqing/Documents/1004/line-modeling_Circinus' # feifei
-modelPath = f'{projectRoot}/data/model_npy_iset'
+projectRoot = '/Users/aqing/Documents/1004/line-modeling_Circinus' # feifei
+modelPath = f'{projectRoot}/data/model_npy'
 mom0Path = f'{projectRoot}/data/regrid_map_nyq'
-emapPath = f'{projectRoot}/data/error_map'
-productPath = f'{projectRoot}/products'
+productPath = f'{projectRoot}/products/fittingResult_notsure'
 
-ndmodel = 6
-data_shape = np.load(
-    f'{mom0Path}/mom0_co-10_smooth3.2as_1.0sigma_regrid.npy'
-    ).shape # i.e. naxis1&2 says image is 89*89 in spatial, data_shape == (89, 89)
-
-### ---------------------------- Basic Variables ---------------------------- ###
+# --------------------- Basic Variables ---------------------- #
 caliError = 0.1 # calibration error, by Eltha
-model_shape = np.load(f'{modelPath}/flux_{ndmodel}d-coarse2_c18o-10.npy').shape # any molename can work
-print(f'Model shape: {model_shape}')
 
 # ((molespiece-transis), 要用 mask 掉多少 sigma 的 mom0)
-moles_info = [('co-10',   1.0),
-              ('13co-10', 1.0),
-              ('co-21',   1.0),
-              ('13co-21', 1.0),
-              ('c18o-21', 1.0),
-              ('co-32',   1.0),
+moles_info = [#('co-10',   3.0),
+              #('13co-10', 3.0),
+              ('co-21',   3.0),
+              ('13co-21', 3.0),
+              #('c18o-21', 1.0),
+              ('co-32',   5.0),
+              ('co-65',   4.0),
              ]
 
-nline = len(moles_info)
+# ------------------------ Load Data ------------------------- #
+'''
+Check every line in this block before fitting.
+Won't take you too much time for checking,
+but re-start a fitting propress wastes time!
+'''
+# Beam size, we have [3.2, 0.41] now.
+bsize = 0.41
+
+data_shape = np.load(
+    f'{mom0Path}/mom0_co-32_smooth{bsize}as_5.0sigma_regrid.npy'
+    ).shape # i.e. naxis1&2 says image is 89*89 in spatial, data_shape == (89, 89)
+print(f'Data shape: {data_shape}, pixel number of image.')
+
+# phyArray, for 反推, only need N12co
+phyArray = np.load(f'{modelPath}/phy_plain-model_Tk-nH2-Nco.npy')
+
+# Beam-filling factor or not
+with_bf = False
+if with_bf:
+    Phib = np.load(f'{modelPath}/phy_plain-model_Phibf.npy')
+    num_para = 4
+else:
+    num_para = 3
+
+# Load Flux Model (.npy)
+model_shape = np.load(
+    f'{modelPath}/flux_plain-model_{num_para}para_c18o-10.npy'
+    ).shape # any molename can work
+print(f'Model shape: {model_shape}, (RADEX physical parameters grid, beam-filling factor grid)')
+
 flux_model = {} # 只裝 model
 for molename, nsig in moles_info: # pixel independent
-    # Load Flux Model (.npy)
-    flux_model[molename] = {"flux_model": np.load(f'{modelPath}/flux_{ndmodel}d-coarse2_{molename}.npy')}
+    flux_model[molename] = {"flux_model": np.load(
+        f'{modelPath}/flux_plain-model_{num_para}para_{molename}.npy')}
 
-### --------------- def: Get Physical Conditions from chi2_min -------------- ###
+# --------------- def: Get Physical Conditions from chi2_min --------------- #
+'''
+input: chi2_array
+然後會用 unravel_index() 找出對應的 index
+然後就可以從 index 反推出對應的 physical condition
+
+bs2phy 是 best set to physical condition 的意思
+有點簡寫了哈
+'''
 def bs2phy(chi2_array):
-    '''
-    best set to physical condition 的意思
-    有點簡寫了哈
-    '''
     if np.all(np.isnan(chi2_array)): # 如果整個 chi2_sum 都是 NaN, 就回傳一組 NaN
-        return (np.nan, np.nan, np.nan, np.nan, np.nan, np.nan)
-
+        return np.full(num_para, np.nan)
     best_set = np.unravel_index(np.nanargmin(chi2_array, axis=None), chi2_array.shape)
-
-    '''eltha's range
-    Nco_best = np.round(0.2 * best_set[0] + 15., 1)
-    Tk_best = 0.1 * best_set[1] + 1.
-    nH2_best = 0.2 * best_set[2] + 2.
-    X1213_best = np.round(10 * best_set[3] + 10., 1)
-    X1318_best = np.round(1 * best_set[4] + 2., 1)
-    Phi_best = np.round(0.05 * best_set[5] + 0.05, 1)
-    return (Nco_best, Tk_best, nH2_best, X1213_best, X1318_best, Phi_best)
     '''
-    #''' _iset range
-    Nco_best = np.round(0.2 * best_set[0] + 12., 1)
-    Tk_best = 0.2 * best_set[1] + 1.
-    nH2_best = 0.2 * best_set[2] + 2.
-    X1213_best = np.round(10 * best_set[3] + 10., 1)
-    X1318_best = np.round(1 * best_set[4] + 2., 1)
-    Phi_best = np.round(0.05 * best_set[5] + 0.05, 1)
-    return (Nco_best, Tk_best, nH2_best, X1213_best, X1318_best, Phi_best)
+    flat_idx = np.nanargmin() # 展平之後, 忽略 nan 尋找最小值的 idx
+    best_set = np.unravel_index() # Turn flatten index into nd array's idx
+                                  # >> shape like: (9152, 14) 
+    '''
+    Tk_best = phyArray[best_set[0]][0]
+    nH2_best = phyArray[best_set[0]][1]
+    Nco_best = phyArray[best_set[0]][2]
+    if with_bf:
+        Phib_best = Phib[best_set[1]] # 因為我 Phibf 是另外存的, 最佳索引在 best_set 的另一邊
+        return [Tk_best, nH2_best, Nco_best, Phib_best]
+    else:
+        return [Tk_best, nH2_best, Nco_best]
 
-### --------------------------- def: Fix ONE pixel -------------------------- ###
+# --------------------------- def: Fix ONE pixel --------------------------- #
 '''
 input:  pixel index (order: y, x)
 output: (pix_y, pix_x, chi2_min, best_phy)
@@ -94,9 +117,9 @@ def fit1pix(pix_y, pix_x):
     # ------------- Get fiiting materials ------------- #
     for molename, nsig in moles_info:
         # Load Real Flux Data from mom0 (.npy)
-        flux_obs = np.load(f'{mom0Path}/mom0_{molename}_smooth3.2as_{nsig}sigma_regrid.npy')[pix_y, pix_x]
+        flux_obs = np.load(f'{mom0Path}/mom0_{molename}_smooth{bsize}as_{nsig}sigma_regrid.npy')[pix_y, pix_x]
         # Import Error Maps (.fits)
-        emap = fits.open(f'{mom0Path}/emap_{molename}_regrid.fits')[0].data[pix_y, pix_x]
+        emap = fits.open(f'{mom0Path}/emap_{molename}_smooth{bsize}as_regrid.fits')[0].data[pix_y, pix_x]
         #** flux_obs & emap 的檢查點 **#
         if np.isnan(flux_obs) or np.isnan(emap):
             pix_permitted = False # 標示為壞 pixel
@@ -119,7 +142,7 @@ def fit1pix(pix_y, pix_x):
 
     # ------------------- GO! or NO GO ------------------ #
     if not pix_permitted:
-        return (pix_y, pix_x, np.nan, [np.nan]*6)
+        return (pix_y, pix_x, np.nan, [np.nan]*num_para) # (y, x, chi2, [Tk, nH2, Nco, Phib])
 
     # --------------- Compute chi^2 Array --------------- #
     # pix_permitted = True 可以上天堂
@@ -127,32 +150,35 @@ def fit1pix(pix_y, pix_x):
     for molename, material_set in local_fitMaterial.items():
         chi2_sum += ((material_set["flux_model"] - material_set["flux_obs"]) / material_set["error"]) ** 2
 
-    # --------------- Get Physical Conditions --------------- #
+    # ------------- Get Physical Conditions ------------- #
     best_phy = bs2phy(chi2_sum)
     chi2_min = np.nanmin(chi2_sum)
     return (pix_y, pix_x, chi2_min, best_phy)
 
 
-### ------------------------------- START! ---------------------------------- ###
-print('READY? GO!')
+# ------------------------------ START! ------------------------------ #
+print('------------------- READY? GO! -------------------')
 
 results = Parallel(n_jobs=-1)( # 用掉所有的緒是不道德的!
     delayed(fit1pix)(y, x)
-    for y in range(data_shape[1]) for x in range(data_shape[0])
+    for y in range(data_shape[0]) for x in range(data_shape[1])
 )
 print('Done!')
 fitTime = time.time()
-print(f'It took {(fitTime - startTime):.2f} seconds to finish fitting whole map:)')
-### --------------------- Fill back to result_arrays -------------------- ###
-map_best_phy = np.full((data_shape[0], data_shape[1], 6), np.nan) # 6 physical condition
-map_chi2_min = np.full(data_shape, np.nan) # 回填同樣大小的陣列到時候要換回 WCS 系列的會稍微方便一點
+print(f'It took {(fitTime - startTime):.2f} seconds to fit whole map:)')
 
-for y, x, chi2_mn, phy_condi in results:
-    map_best_phy[y, x, :] = phy_condi
+# --------------- Fill back to result_arrays --------------- #
+map_best_phy = np.full((data_shape[0], data_shape[1], num_para), np.nan)
+map_chi2_min = np.full(data_shape, np.nan)
+
+for y, x, chi2_mn, best_phy in results:
+    map_best_phy[y, x] = best_phy
     map_chi2_min[y, x] = chi2_mn
 
-np.save(f'{productPath}/fittingResult_notsure/map_chi2Min_{nline}line_wholemap.npy', map_chi2_min)
-np.save(f'{productPath}/fittingResult_notsure/map_bestPhyCondi_{nline}line_wholemap.npy', map_best_phy)
+np.save(f'{productPath}/map_chi2Min_{num_para}para_resolved.npy',
+        map_chi2_min)
+np.save(f'{productPath}/map_bestPhyCondi_{num_para}para_resolved.npy',
+        map_best_phy)
 print('Results are saved as .npy files.')
 endTime = time.time()
 print(f'It took {(endTime - fitTime):.2f} seconds to fill back the result arrays.')
