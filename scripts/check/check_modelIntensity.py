007c69b91c6b3ead8571e63fa66abf29cdb6b24e
Liv <universe5961@gmail.com>
2026-09-20 21:33:21 +0800

    pass the test of mode `regular`

--- scripts/check/check_modelIntensity.py
+++ scripts/check/check_modelIntensity.py
@@ -6,6 +6,7 @@ Physical parameters obtained from fitting correspond to which model flux?
 也許是我要瞎了
 
 update: 2026-09-19: Fix dimension problem and generalize this script.
+        2026-09-20: Add residual map of predicted flux and observed data.
 '''
 
 import matplotlib.pyplot as plt
@@ -27,17 +28,17 @@ if mode == 'regular':
     modelPath = f'{projectRoot}/data/model_no-hco+'
     phyArray = np.load(f'{modelPath}/phy_plain-model_Tk-nH2-Nco.npy')
     bsize = 3.2
-    mom0Path = f'{projectRoot}/data/regrid_map_nyq'
+    regridPath = f'{projectRoot}/data/regrid_map_nyq'
     resultPath = f'{projectRoot}/products/fittingResult_newscript'
     best_phy = np.load(f'{resultPath}/map_bestPhyCondi_{num_para}para_newlw.npy')
-    moles_info = [('co-10',   3.0, 2400), ('13co-10', 3.0, 1000),
-                  ('co-21',   3.0, 2500), ('13co-21', 3.0, 450),
-                  ('c18o-21', 3.0, 1000), ('co-32',   3.0, 2400)]
+    moles_info = [('co-10',   3.0, 1100), ('13co-10', 3.0, 70),
+                  ('co-21',   3.0, 1200), ('13co-21', 3.0, 180),
+                  ('c18o-21', 3.0, 35),   ('co-32',   3.0, 1000)]
 elif mode == 'resolved':
     modelPath = f'{projectRoot}/data/model_no-hco+' # model grid 沒有 re不resolve 的問題
     phyArray = np.load(f'{modelPath}/phy_plain-model_Tk-nH2-Nco.npy')
     bsize = 0.41
-    mom0Path = f'{projectRoot}/data/regrid_map_resolved'
+    regridPath = f'{projectRoot}/data/regrid_map_resolved'
     resultPath = f'{projectRoot}/products/fittingResult_resolved'
     best_phy = np.load(f'{resultPath}/map_bestPhyCondi_{num_para}para_resolved.npy')
     moles_info = [('co-21',   3.0, 2500), ('13co-21', 3.0, 450),
@@ -56,8 +57,9 @@ datashape = best_phy.shape[:2] # Equals to any mole's mom0(observe) shape. (358,
 for molename, nsig, _ in moles_info:
     fluxx[molename] = {
         "model":   np.load(f'{modelPath}/flux_plain-model_{num_para}para_{molename}.npy'),
-        "observe": np.load(f'{mom0Path}/mom0_{molename}_smooth{bsize}as_{nsig}sigma_regrid.npy'),
-        "predict": np.full(datashape, np.nan)
+        "observed": np.load(f'{regridPath}/mom0_{molename}_smooth{bsize}as_{nsig}sigma_regrid.npy'),
+        "error": np.load(f'{regridPath}/emap_{molename}_smooth{bsize}as_regrid.npy'),
+        "predicted": np.full(datashape, np.nan)
     }
 
 # ----------------- Find correct index in phyArray ----------------- #
@@ -106,21 +108,32 @@ validIdx_phib = matchIdx_phib[validMask].astype(int)
 
 for molename in fluxx.keys():
     mflux = fluxx[molename]["model"]
-    fluxx[molename]["predict"][validMask] = mflux[validIdx_phy, validIdx_phib]
+    fluxx[molename]["predicted"][validMask] = mflux[validIdx_phy, validIdx_phib]
     '''
     fuck i dont actually under stand why i do this
     but it works so i dot car
     '''
 
 # ------------------------  Make the figures ----------------------- #
-fig, ax = plt.subplots(2, 3, figsize=(14, 7)) # 存下來就好看了
+# Predicted flux of each mole
+fig, ax = plt.subplots(2, 3, figsize=(14, 7))
 ax_flat = ax.flatten() # 壓成 1d 這樣可以用洄圈
-
 for i, m in enumerate(moles_info):
-    splot = ax_flat[i].imshow(fluxx[m[0]]["predict"],
+    splot = ax_flat[i].imshow(fluxx[m[0]]["predicted"],
                               vmax=m[2], origin='lower')
-    ax_flat[i].set_title(f'predicted flux of {m[0]}')
+    ax_flat[i].set_title(f'Predicted flux of {m[0]}')
     cbar = fig.colorbar(splot, ax=ax_flat[i], fraction=0.046, pad=0.04) # 精心調整的小數值
-
 plt.tight_layout() # 神奇妙妙工具
+
+# Normalized residual and residual
+fig, ax = plt.subplots(2, 3, figsize=(14, 7))
+ax_flat = ax.flatten() # 壓成 1d 這樣可以用洄圈
+for i, m in enumerate(moles_info):
+    residual = fluxx[m[0]]["predicted"] - fluxx[m[0]]["observed"]
+    residual_nor = residual / fluxx[m[0]]["error"]
+    splot = ax_flat[i].imshow(residual_nor, cmap='coolwarm', origin='lower') # 怎麼弄到已0為中心啊屁ㄧ
+    ax_flat[i].set_title(f'{m[0]} residudal map')
+    cbar = fig.colorbar(splot, ax=ax_flat[i], fraction=0.046, pad=0.04) # 精心調整的小數值
+plt.tight_layout() # 神奇妙妙工具
+
 plt.show()
